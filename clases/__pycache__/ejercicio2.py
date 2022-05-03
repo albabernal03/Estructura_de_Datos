a93e780f9994c79725d0eb2148ce808d8f5074a8
albabernal03 <ALBABERO@MYUAX.COM>
2022-05-03 20:17:49 +0200

    terminamos de crear el ejercicio 2

--- clases/__pycache__/ejercicio2.py
+++ clases/__pycache__/ejercicio2.py
@@ -1,2 +1,30 @@
 import time
 import os
+
+class MVC:
+    def __init__(self, linea1, linea2):
+        self.linea1 = linea1
+        self.linea2 = linea2
+    def mensaje(self):
+        linea1 = input('Introduzca una frase:')
+        linea2 = input('Introduzca otra frase:')
+        #A continuacion convertimos a mayusculas las lineas y comprobamos que son cadeas de caracteres
+        if isinstance(linea1, str) and isinstance(linea2, str): #Comprobamos que las lineas son cadeas de caracteres con isinstance el cual devuelve un booleano
+            linea1 = linea1.upper()
+            linea2 = linea2.upper()
+        else:
+            print('No es una cadena de caracteres')
+            return
+    def fichero(self):
+        #A continuacion creamos el fichero
+        fichero = open('ejercicio2.txt', 'w')
+        #A continuacion escribimos en el fichero
+        fichero.write(self.linea1)
+        fichero.write('\n')
+        fichero.write(self.linea2)
+        #Cerramos el archivo en 10 segundos
+        time.sleep(10)
+        fichero.close()
+        os.remove(fichero)
+
+    
